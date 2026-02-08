f0d4c0bc191cef702ce2ec84b35518a8a58728a2
Eric Gorkow <eric.gorkow@gmx.de>
2026-02-08 23:12:29 +0100

    Delete unused config

--- lib/config.dart
+++ lib/config.dart
@@ -1,8 +1,3 @@
 class Config {
-  static const String serverUrl = 'serverUrl';
-  static const String username = 'username';
-  static const String password = 'password';
   static const String appTitle = 'Nextcloud Uploader';
-
-  static const String defaultUploadPath = '/';
 }
